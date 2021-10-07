772aa68d6524c898b30824d5a4cb036caa6849ba
Rinyaresu <kaiquelinhares17@outlook.com>
2021-10-07 02:53:35 -0300

    ordenação asc para desc

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,6 @@
 class ProdutosController < ApplicationController
   def index
-    @produtos = Produto.order :nome 
+    @produtos = Produto.order(nome: :desc).limit 2
     @produtos_com_desconto = Produto.order(:preco).limit 1
   end
 
